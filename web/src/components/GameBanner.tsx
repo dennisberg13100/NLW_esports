2bb4140c82430d767fff51e2ec10509f4e904a6a
Dennis van den Berg <dennisvandenberg13100@gmail.com>
2022-09-15 12:11:08 -0300

    metade da 4 aula completada - web

---
+++ web/src/components/GameBanner.tsx
@@ -0,0 +1,22 @@
+interface GameBannerProps {
+    bannerUrl: string
+    title: string
+    adsCount: number
+
+}
+
+export function GameBanner(props: GameBannerProps) {
+    return (
+        <a href="" className="relative rounded-lg overflow-hidden">
+            <img src={props.bannerUrl} alt="" className="w-full"/>
+            <div className="m-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 ">
+            <strong className="text-white block">
+                {props.title}
+            </strong>
+            <span  className="text-zinc-300 text-sm block mt-1">
+                {props.adsCount} anúncio(s)
+            </span>
+            </div>
+        </a>
+    )
+}
